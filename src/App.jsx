f3ae6928900e1ecf1f0a9889353ff94380113c95
Khushi Jaiswar <khushijaiswar77049@gmail.com>
2024-07-17 13:44:54 +0530

    India-Clock-Time

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ClockHeading from './ClockHeading';
+import ClockSlogan from './ClockSlogan';
+import CurrentTime from './CurrentTime';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+  
+  return <div>
+    <ClockHeading></ClockHeading>
+    <ClockSlogan></ClockSlogan>
+    <CurrentTime></CurrentTime>
+  </div>
+}
+
+export default App
